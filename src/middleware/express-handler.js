c094dc520e2da7296a9526a3133799d4c695167e
programadorisgod <catapiascastro@gmail.com>
2025-09-15 17:29:14 -0500

    fix: add content-type application/problem+json to response

--- src/middleware/express-handler.js
+++ src/middleware/express-handler.js
@@ -6,10 +6,10 @@ export function problemDetailsHandler() {
     if (err instanceof ProblemDetailsError) {
       const response = err.toJSON();
       response.instance = req.originalUrl;
-      res.status(err.status).json(response);
+      res.status(err.status).type('application/problem+json').json(response);
     }
 
-    return res.status(500).json({
+    return res.status(500).type('application/problem+json').json({
       type: "about:blank",
       title: "Internal Server Error",
       status: statusCodes.INTERNAL_SERVER_ERROR,
